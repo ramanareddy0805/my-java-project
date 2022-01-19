18421ee6dcfeeb7b82ccb772bed8cfe4991e0975
ramanareddy0805 <88232571+ramanareddy0805@users.noreply.github.com>
2022-01-19 08:23:34 +0530

    Add files via upload

---
+++ pepjavaday1.java
@@ -0,0 +1,13 @@
+//In java -> program has a class
+//        -> class will have a main method
+//        -> so waht ever we write in the main method, it will be excuted.
+class ram{
+    // whatever we wish the program to do, will go inside the main.
+    public static void main(String[] args){ 
+        // string[] args  -> command line arguements.
+        System.out.println("our goal is to develop solution to save time");
+        System.out.println("our goal is to develop solution whaich are accurate");
+
+       
+    }
+}
